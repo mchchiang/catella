4620c5f2b7dd16f189ac9c595694d43376b36534
Michael Chiang <michael.chiang@ed.ac.uk>
2026-05-26 12:07:52 +0100

    Add program summary

---
+++ src/nucmc/__about__.py
@@ -0,0 +1,12 @@
+# __about__.py
+
+__version__ = "0.1.0"
+__author__ = "Michael Chiang"
+__email__ = "michael.chiang@ed.ac.uk"
+__maintainers__ = [
+    {"name": "Davide Marenduzzo"},
+    {"name": "Nick Gilbert"},
+]
+__summary__ = "A Monte-Carlo simulator for predicting nucleosome positions" \
+    "based on DNA methylation footprinting data"
+__url__ = "https://git.ecdf.ed.ac.uk/cchiang2/nucmc-project"
